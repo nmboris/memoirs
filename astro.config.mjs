17e8ef9ce2816773499036260714bc0525de5587
Boris Brenner <63914278+nmboris@users.noreply.github.com>
2023-10-03 20:02:25 +0200

    added vercel; moved to yarn, pnpm causes problems

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	output: 'server'
+  site: 'https://example.com',
+  output: 'server',
+  adapter: vercel()
 });
